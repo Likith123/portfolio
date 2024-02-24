dd2fb02d173bf563d4afccd3c98193d0c6fddd9a
Likith Naga Sai Adusumalli <likithadusumalli@gmail.com>
2024-02-25 00:43:08 +0530

    Updatedcode to the latest version of React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { RouterProvider, createBrowserRouter} from "react-router-dom";
+
+import PageOutlet from "./components/PageOutlet";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+const router = createBrowserRouter([
+    { path: "/", element: <PageOutlet />, children: [      
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "projects", element: <Projects /> },
+        { path: "contactMe", element: <Contact /> }
+    ] },
+    
+]);
+
 function App() {
     return (
-        <BrowserRouter>
-            <Navbar />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/contactMe" element={<Contact />} />
-                </Routes>
-            </main>
-        </BrowserRouter>
+        <RouterProvider router = {router}/>
     );
 }
 
---
+++ src/components/PageOutlet.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function PageOutlet() {
+    return <>
+        <Navbar/>
+        <main>
+            <Outlet/>
+        </main>
+    </>;
+}
+
+export default PageOutlet;
